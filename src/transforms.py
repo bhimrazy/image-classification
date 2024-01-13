a69444191a0e1fb6a59d89d5441cff15b9c556bf
bhimrazy <bhimrajyadav977@gmail.com>
2024-01-13 19:46:12 +0545

    Add color jitter transformation to train_transform

--- src/transforms.py
+++ src/transforms.py
@@ -5,9 +5,9 @@ train_transform = T.Compose(
     [
         T.Resize((IMAGE_SIZE, IMAGE_SIZE)),
         T.RandomAffine(degrees=10, translate=(0.1, 0.1), scale=(0.8, 1.2)),
+        T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.01),
         # T.RandomHorizontalFlip(),
         # T.RandomRotation(10),
-        # T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
         T.ToTensor(),
         T.Normalize(
             [0.485, 0.456, 0.406],  # mean
