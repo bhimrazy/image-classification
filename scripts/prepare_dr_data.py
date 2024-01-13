55c61158fa518580db3945f0cd553bccffeb82b1
bhimrazy <bhimrajyadav977@gmail.com>
2024-01-13 20:34:39 +0545

    Add command line arguments to prepare_dr_data.py

--- scripts/prepare_dr_data.py
+++ scripts/prepare_dr_data.py
@@ -1,6 +1,22 @@
+import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+
+parser = argparse.ArgumentParser(description="Prepare the DR dataset.")
+parser.add_argument(
+    "--num-samples",
+    type=int,
+    default=100,
+    help="The number of samples per class.",
+)
+parser.add_argument(
+    "--test-size",
+    type=float,
+    default=0.2,
+    help="The test size.",
+)
+args = parser.parse_args()
 
 # Read the data
 df = pd.read_csv("data/dr-data-kaggle/trainLabels.csv.zip")
@@ -12,8 +28,7 @@ df["image"] = "data/dr-data-kaggle/images/" + df["image"] + ".jpeg"
 df.rename(columns={"image": "image_path", "level": "label"}, inplace=True)
 
 # drop extra data from the dataframe by limiting max of 100 samples from each class
-N = 100
-df = df.groupby("label").head(N).reset_index(drop=True)
+df = df.groupby("label").head(args.num_samples).reset_index(drop=True)
 
 # plot the distribution of the labels
 df["label"].value_counts().plot.bar()
@@ -22,7 +37,7 @@ plt.savefig("data/dr-data-kaggle/label_distribution.png")
 
 # split the data into train and test
 train_df, test_df = train_test_split(
-    df, test_size=0.2, random_state=42, stratify=df["label"], shuffle=True
+    df, test_size=args.test_size, random_state=42, stratify=df["label"], shuffle=True
 )
 
 # print data distribution
