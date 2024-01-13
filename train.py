9979ba8be183f5722a54d6c52c1a2e29d1c9df61
bhimrazy <bhimrajyadav977@gmail.com>
2024-01-13 21:14:21 +0545

    Add Tensorboard logging for training results

--- train.py
+++ train.py
@@ -1,4 +1,9 @@
+import os
+from datetime import datetime
+
 import torch
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 from src.data_loader import create_data_loader
@@ -6,6 +11,10 @@ from src.model import DenseNetModel
 from src.parser import parse_args
 from src.utils import get_device, seed_everything, train_eval_step
 from src.viz import plot_results
+
+runs_dir = 'runs'
+os.makedirs(runs_dir, exist_ok=True)
+
 
 
 def main() -> None:
@@ -91,6 +100,32 @@ def main() -> None:
         torch.save(model.state_dict(), "model.pth")
 
     plot_results(results)
+
+    # Tensorboard
+    run_name = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+    writer = SummaryWriter(os.path.join(runs_dir, run_name))
+
+    # log results
+    writer.add_scalars('loss', {'train': results["train_loss"], 'test': results["test_loss"]}, 0)
+    writer.add_scalars('accuracy', {'train': results["train_acc"], 'test': results["test_acc"]}, 0)
+
+    # log model
+    writer.add_graph(model, train_loader.dataset[0][0].unsqueeze(0).to(device))
+
+    # log images
+    images, labels = next(iter(train_loader))
+    grid = torchvision.utils.make_grid(images)
+    writer.add_image('images', grid, 0)
+
+    # log hyperparameters
+    writer.add_hparams(
+        {'lr': args.lr, 'batch_size': args.batch_size, 'epochs': args.epochs},
+        {'hparam/accuracy': results["test_acc"][-1], 'hparam/loss': results["test_loss"][-1]}
+    )
+
+    # close writer
+    writer.close()
+
 
 
 if __name__ == "__main__":
